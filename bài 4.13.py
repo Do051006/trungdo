8acf6bba107abb637c3bda076527f49bbfda6a6e
Do051006 <duchoangminh665@gmail.com>
2025-11-11 20:17:45 +0700

    Add files via upload

---
+++ bài 4.13.py
@@ -0,0 +1,4 @@
+ds = ["xyz", "abc", "def", "ghi"]
+# Sử dụng phương thức .index() để tìm vị trí (chỉ mục) của phần tử đầu tiên
+vi_tri = ds.index("abc")
+print(f"Vị trí của chuỗi 'abc' là: {vi_tri}")
