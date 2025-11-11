3aa9e739d301000e6759522f15c58ec7fe6854e0
Do051006 <duchoangminh665@gmail.com>
2025-11-11 20:11:43 +0700

    Add files via upload

---
+++ bài 4.11.py
@@ -0,0 +1,9 @@
+ds = ["apple", "banana", "cat"]
+print("List gốc:", ds)
+
+# Sử dụng phương thức .append() để thêm phần tử vào cuối list
+ds.append('abc')
+
+print("List sau khi thêm:")
+for ch in ds:
+    print(ch)
