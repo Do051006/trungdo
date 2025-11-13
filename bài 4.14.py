41f84d6c05d5ccf10a8f265cd36d10e87a5d414e
Do051006 <duchoangminh665@gmail.com>
2025-11-13 12:46:27 +0700

    Delete bài 4.14.py

--- bài 4.14.py
+++
@@ -1,8 +0,0 @@
-ds = [5, 2, 8, 1, 9, 4]   # Tạo danh sách gồm các số nguyên
-
-# Sử dụng phương thức .sort() để sắp xếp list tại chỗ (theo thứ tự tăng dần mặc định)
-ds.sort()
-
-print("List sau khi sắp xếp:")
-for ch in ds:
-    print(ch)
