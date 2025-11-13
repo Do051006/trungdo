d00cc654849d12ff6a7cd812ca77fd58a2f64a75
Do051006 <duchoangminh665@gmail.com>
2025-11-13 12:45:07 +0700

    Delete bài 4.11.py

--- bài 4.11.py
+++
@@ -1,9 +0,0 @@
-ds = ["apple", "banana", "cat"]
-print("List gốc:", ds)
-
-# Sử dụng phương thức .append() để thêm phần tử vào cuối list
-ds.append('abc')
-
-print("List sau khi thêm:")
-for ch in ds:
-    print(ch)
