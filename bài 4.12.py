12d3647539423b9f47d11c02652120807d686491
Do051006 <duchoangminh665@gmail.com>
2025-11-13 12:45:36 +0700

    Delete bài 4.12.py

--- bài 4.12.py
+++
@@ -1,9 +0,0 @@
-ds = ["123", "456", "789", "123"]
-print("List gốc:", ds)
-
-# Sử dụng phương thức .remove() để xóa phần tử đầu tiên có giá trị là '123'
-ds.remove('123')
-
-print("List sau khi xóa:")
-for ch in ds:
-    print(ch)
