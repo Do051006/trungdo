af9d04d7077e73f5aab5b8c290554ee088b5e4f0
Do051006 <duchoangminh665@gmail.com>
2025-11-13 12:55:58 +0700

    Delete th1 b8.py

--- th1 b8.py
+++
@@ -1,9 +0,0 @@
-a, b = 1, 2
-total = 0
-print(a, end=" ")
-while a <= 4000000:
-    if a % 2 == 0:
-        total += a
-    print(a, end=" ")
-    a, b = b, a + b
-print("\nTổng các số chẵn trong dãy Fibonacci là:", total)
