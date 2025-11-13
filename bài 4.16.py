286eab747636a1ef036e17773cf29268dd44cb6c
Do051006 <duchoangminh665@gmail.com>
2025-11-13 12:47:06 +0700

    Delete bài 4.16.py

--- bài 4.16.py
+++
@@ -1,8 +0,0 @@
-# 1. Tách chuỗi thành một list các số nhị phân
-binary_numbers = input_str.split(',')
-
-# 2. In ra các giá trị đã được nhập
-print("\nCác số nhị phân đã nhập:")
-for binary in binary_numbers:
-    # Sử dụng .strip() để loại bỏ khoảng trắng thừa nếu có
-    print(binary.strip())
